7dc9c70abd58f0637c2d50acffbde1b3ec853b6b
Enrique Austin <antonio123bo2@gmail.com>
2023-09-14 21:54:18 +0200

    updates

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <h1>Two Trees Olive Oil</h1>
+  <NavBar />
+  
   <div class="page-wrap">
     <router-view></router-view>
   </div>
@@ -7,9 +8,12 @@
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue';
 
 export default {
   name: 'App',
-  components: {}
-}
+  components: {
+    NavBar,
+  },
+};
 </script>
--- front-end/src/pages/ShoppingCartPage.vue
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -1,18 +1,21 @@
 <template>
     <h1>Shopping Cart</h1>
-    <div 
+
+    <div  v-if="cartItems.length > 0" >
+        <div
         class="product-container" 
         v-for="product in cartItems" 
         :key="product.id"
     >
-        <img class="product-image" :src="product.image" />
-        <div class="details-warp">
+        <img class="product-image" :src="product.imageName" />
+        <div class="details-wrap">
             <h3>{{ product.name }}</h3>
             <p>{{ product.price }}</p>
         </div>
         <button class="remove-button">Remove from Cart</button>
     </div>
     <button class="checkout-button">Proceed to Checkout</button>
+    </div>
 </template>
 
 <script>
